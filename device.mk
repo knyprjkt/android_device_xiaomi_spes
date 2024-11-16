5c67abcee7867a2856b3c2a2c27adcf15ae79b9b
angelomds42 <angelomds42@gmail.com>
2024-11-16 07:46:38 -0300

    spes: Move thermal settings to device tree
    Signed-off-by: angelomds42 <angelomds42@gmail.com>

--- device.mk
+++ device.mk
@@ -497,6 +497,10 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
   android.hardware.thermal-service.qti
 
+# Thermal configs
+PRODUCT_COPY_FILES += \
+  $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/thermal/,$(TARGET_COPY_OUT_VENDOR)/etc)
+
 # Update Engine
 PRODUCT_PACKAGES += \
     update_engine \
