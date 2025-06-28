64a12aae9ac6f5c941ec5401228a26b137a27931
angelomds42 <angelomds42@gmail.com>
2025-06-28 07:50:21 -0300

    spes: Import systemui preopt otimizations from hw pixel
    - https://android.googlesource.com/platform/hardware/google/pixel/+/8612656e216075f79a1efa3bb9be6b6943d6e8cc/common/pixel-common-device.mk
    
    Signed-off-by: angelomds42 <angelomds42@gmail.com>

--- device.mk
+++ device.mk
@@ -358,6 +358,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
+# Preopt SystemUI
+PRODUCT_DEXPREOPT_SPEED_APPS += SystemUIGoogle  # For internal
+PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI        # For AOSP
+
 # QMI
 PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti \
